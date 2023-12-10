6717536deec2ac55f0f1ddf64f83080fcea0375b
Wer0nikaNiewiad0mska <146941847+Wer0nikaNiewiad0mska@users.noreply.github.com>
2023-12-10 19:08:27 +0100

    Update script.js

--- code/script.js
+++ code/script.js
@@ -17,4 +17,23 @@ function scrollGallery()
   }
 }
 
+let mybutton = document.getElementById("myBtn");
+
+// Gdy użytkownik przejdzie w dół 30px, pojawi się przycisk
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Jak użytkownik kliknie w przycisk, strona pójdzie w górę
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 setInterval(scrollGallery, 1); // Funkcja scrollGallery wywoływana co 1ms dla płynności
