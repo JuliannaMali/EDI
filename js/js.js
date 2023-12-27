fa0fb9117c9a4eb33e55da08da6e68b4b838040b
Weronika Niewiadomska <146941847+Wer0nikaNiewiad0mska@users.noreply.github.com>
2023-12-27 23:50:41 +0100

    znikanie nawigacji

--- js/js.js
+++ js/js.js
@@ -34,20 +34,39 @@ function ()
   }
 });
 let mybutton = document.getElementById("myBtn");
+let prevScrollPos = window.scrollY;
+const navbar = document.getElementById('myNavbar');
 
-      // Gdy użytkownik przejdzie w dół 30px, pojawi się przycisk
-      window.onscroll = function() {scrollFunction()};
-      
-      function scrollFunction() {
-        if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-          mybutton.style.display = "block";
-        } else {
-          mybutton.style.display = "none";
-        }
-      }
-      
-      // Jak użytkownik kliknie w przycisk, strona pójdzie w górę
-      function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-      };
+window.onscroll = function() {
+  // Gdy użytkownik przejdzie w dół 30px, pojawi się przycisk
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+
+  // Obsługa znikania nawigacji
+  let currentScrollPos = window.scrollY;
+
+  if (currentScrollPos > 30) {
+    // Pokaż nawigację tylko gdy przewinięcie jest w dół i użytkownik jest wystarczająco w dół strony
+    if (prevScrollPos > currentScrollPos) {
+      // Przewinięcie w górę
+      navbar.classList.remove('hidden');
+    } else {
+      // Przewinięcie w dół
+      navbar.classList.add('hidden');
+    }
+  } else {
+    // Ustaw nawigację jako widoczną, gdy użytkownik jest blisko góry strony
+    navbar.classList.remove('hidden');
+  }
+
+  prevScrollPos = currentScrollPos;
+};
+
+// Jak użytkownik kliknie w przycisk, strona pójdzie w górę
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
