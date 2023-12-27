03839d30c51a6e4e7af9f55f93cd4c4e1db484b1
Weronika Niewiadomska <146941847+Wer0nikaNiewiad0mska@users.noreply.github.com>
2023-12-27 23:34:01 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -50,4 +50,4 @@ let mybutton = document.getElementById("myBtn");
       function topFunction() {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
-      }
+      };
